610b2fab19d835289a407818741e02be89593783
hassangulzar1 <hassangulzar121@gmail.com>
2023-07-26 19:01:57 +0500

    first commit

---
+++ passwordChecker.js
@@ -0,0 +1,26 @@
+let passwords = ["hassangulzar", "barood3310", "clashking"];
+let login = (pass) => {
+  return passwords.includes(pass);
+};
+let returnPromise = (pass) => {
+  return new Promise((resolve, reject) => {
+    if (login(pass)) {
+      resolve(true);
+    } else {
+      reject(false);
+    }
+  });
+};
+
+let checkingPass = (pass) => {
+  returnPromise(pass)
+    .then((value) => {
+      console.log(value);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+checkingPass("hassangulzar");
+checkingPass("124412");
